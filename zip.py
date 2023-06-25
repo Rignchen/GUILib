5a3cfac8294f6ec40374c41a2273af9f86b032bd
Rignchen <119439839+Rignchen@users.noreply.github.com>
2023-06-25 23:41:01 +0200

    update zip.py

--- zip.py
+++ zip.py
@@ -1,24 +1,61 @@
 import zipfile
+import threading
 from os import listdir, getcwd, remove
-def compress(zip_name:str = getcwd().replace("\\", "/").split("/")[-1],unwanted:list=[],path:str="",is_first:bool=True,file:zipfile.ZipFile|None = None):
+
+#making it run simultaniously
+class ZipThread(threading.Thread):
+	def __init__(self, zip_name:str = getcwd().replace("\\", "/").split("/")[-1], wanted:list = [], path:str = "", is_first:bool = True, file:zipfile.ZipFile|None = None, init_path:str = "",compress_files: bool = False):
+		threading.Thread.__init__(self)
+		self.threadID, self.name = 0, "name"
+		self.zip = {"zip_name": zip_name, "wanted": wanted, "path": path, "is_first": is_first, "file": file, "init_path": init_path, "compress_files": compress_files}
+	def run(self):
+		compress(self.zip["zip_name"],self.zip["wanted"],self.zip["path"],self.zip["is_first"],self.zip["file"],self.zip["init_path"],self.zip["compress_files"])
+		print(f"{self.zip['zip_name']} done")
+def start(zip_name:str = getcwd().replace("\\", "/").split("/")[-1], wanted:list = [], path:str = "", is_first:bool = True, file:zipfile.ZipFile|None = None, init_path:str = "",compress_files: bool = False):
+	tred = ZipThread(zip_name, wanted, path, is_first, file, init_path, compress_files)
+	tred.start()
+#compressing the folder
+def compress_file(path: str):
+	file = open(path,"r")
+	text = file.read().split("\n")
+	file.close()
+	out = ""
+	match path.split(".")[-1]:
+		case "json"|"mcmeta":
+			for a in text: out += a.strip()
+		case "mcfunction":
+			for a in text:
+				b = a.strip()
+				if b != "" and b[0] != "#" :out += b + "\n"
+			out = out[:-1]
+		case _:
+			for a in text: out += a + "\n"
+			out = out[:-1]
+	file = open("temp.txt","w+")
+	file.write(out)
+	file.close()
+def compress(zip_name:str = getcwd().replace("\\", "/").split("/")[-1], wanted:list = [], path:str = "", is_first:bool = True, file:zipfile.ZipFile|None = None, init_path:str = "",compress_files: bool = False):
+	if path and not path.endswith(("/", "\\")): path +=  "/"
 	if is_first:
-		all_folder = listdir()
-		if not zip_name.endswith(".zip"): zip_name += ".zip"
-		if zip_name in all_folder: remove(zip_name)
-		file = zipfile.ZipFile(zip_name,"x")
-		unwanted += ["zip.py", ".git", ".gitattributes", ".gitignore",zip_name]
-		for a in unwanted:
-			if a in all_folder:all_folder.remove(a)
+		init_path = path
+		all_folder = wanted
+		if not zip_name.endswith(".zip"): zip_name +=  ".zip"
+		if zip_name in listdir(): remove(zip_name)
+		file = zipfile.ZipFile(zip_name, "x", compression=zipfile.ZIP_DEFLATED, compresslevel=9)
 	else:
-		if not path.endswith(("/", "\\")): path += "/"
 		all_folder = listdir(path)
 	
 	for a in all_folder:
 		try:
 			listdir(path + a)
-			compress(zip_name=zip_name, path=path+a, is_first=False, file=file)
+			compress(zip_name = zip_name, path = path+a, is_first = False, file = file,init_path=init_path,compress_files=compress_files)
 		except NotADirectoryError:
-			file.write(path + a)
+			if compress_files: 
+				compress_file(path + a)
+				file.write("temp.txt", (path + a)[len(init_path):])
+			else: file.write(path + a, (path + a)[len(init_path):])
 	if is_first:
 		file.close()
-compress(unwanted=["stats.py","version_updater.py","todo.txt","all versions"])
+		if compress_files: remove("temp.txt")
+
+start(wanted=["data","LICENSE","pack.mcmeta","syntaxe.json",],compress_files=True)
